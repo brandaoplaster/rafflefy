930329b580522b06f071d521b14a57d67a410ff0
brandaoplaster <brandaoplaster@gmail.com>
2024-12-08 19:05:35 -0300

    feat: Adding handle event add and button

--- lib/rafflefy_web/live/estimator_live.ex
+++ lib/rafflefy_web/live/estimator_live.ex
@@ -4,12 +4,21 @@ defmodule RafflefyWeb.EstimatorLive do
   def mount(_params, _session, socket) do
     {:ok, assign(socket, tickets: 0, price: 3)}
   end
-  
+
+  def handle_event("add", %{"quantity" => quantity}, socket) do
+    socket = update(socket, :tickets, &(&1 + String.to_integer(quantity)))
+
+    {:noreply, socket}
+  end
+
   def render(assigns) do
     ~H"""
       <div class="estimator">
         <h1>Rafflefy Estimator</h1>
         <section>
+          <button phx-click="add" phx-value-quantity="5">
+            +
+          </button>
           <div>
             <%= @tickets %>
           </div>
