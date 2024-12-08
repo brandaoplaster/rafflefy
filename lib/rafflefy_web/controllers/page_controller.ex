64bcf8369e9e7f71727d9791fba7bc922df56945
brandaoplaster <brandaoplaster@gmail.com>
2024-12-07 22:39:15 -0300

    refactor: Refactoring folder structure to html

--- lib/rafflefy_web/controllers/page_controller.ex
+++ lib/rafflefy_web/controllers/page_controller.ex
@@ -2,8 +2,6 @@ defmodule RafflefyWeb.PageController do
   use RafflefyWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
+    render(conn, :home)
   end
 end
