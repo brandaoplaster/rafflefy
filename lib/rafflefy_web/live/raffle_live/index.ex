69c2dcaed9995094e8330ce3c3dd03dbc6397436
brandaoplaster <brandaoplaster@gmail.com>
2024-12-13 23:02:35 -0300

    feat: Adding navigate in rafflefy index

--- lib/rafflefy_web/live/raffle_live/index.ex
+++ lib/rafflefy_web/live/raffle_live/index.ex
@@ -11,21 +11,21 @@ defmodule RafflefyWeb.RaffleLive.Index do
 
   def render(assigns) do
     ~H"""
-    <div class="raffle-index">
-      <.banner>
-        <.icon name="hero-sparkles-solid" />
-        Mystery Raffle Coming Soon!!
-        <:details :let={vibe}>
-          To Be Revealed Tomorrow <%= vibe %>
-        </:details>
-        <:details>
-          Any guesses?
-        </:details>
-      </.banner>
-      <div class="raffles">
-        <.raffle_card :for={raffle <- @raffles} raffle={raffle} />
+      <div class="raffle-index">
+        <.banner>
+          <.icon name="hero-sparkles-solid" />
+          Mystery Raffle Coming Soon!!
+          <:details :let={vibe}>
+            To Be Revealed Tomorrow <%= vibe %>
+          </:details>
+          <:details>
+            Any guesses?
+          </:details>
+        </.banner>
+        <div class="raffles">
+          <.raffle_card :for={raffle <- @raffles} raffle={raffle} />
+        </div>
       </div>
-    </div>
     """
   end
 
@@ -51,16 +51,18 @@ defmodule RafflefyWeb.RaffleLive.Index do
 
   def raffle_card(assigns) do
     ~H"""
-      <div class="card">
-        <img src={@raffle.image_path} />
-        <h2><%= @raffle.prize %></h2>
-        <div class="details">
-          <div class="price">
-            $<%= @raffle.ticket_price %> / ticket
+      <.link navigate={~p"/raffles/#{@raffle}"}>
+        <div class="card">
+          <img src={@raffle.image_path} />
+          <h2><%= @raffle.prize %></h2>
+          <div class="details">
+            <div class="price">
+              $<%= @raffle.ticket_price %> / ticket
+            </div>
+            <.badge status={@raffle.status} />
           </div>
-          <.badge status={@raffle.status} />
         </div>
-      </div>
+      </.link>
     """
   end
 end
