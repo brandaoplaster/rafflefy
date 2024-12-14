81e4b9c717cfdd40d1f09553eaa26acc94c7b22c
brandaoplaster <brandaoplaster@gmail.com>
2024-12-14 14:20:06 -0300

    feat: Adding migrate and schema

---
+++ lib/rafflefy/raffles/raflle.ex
@@ -0,0 +1,23 @@
+defmodule Rafflefy.Raffles.Raflle do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "raffles" do
+    field :status, Ecto.Enum, values: [:upcoming, :open, :closed]
+    field :description, :string
+    field :prize, :string
+    field :ticket_price, :integer
+    field :image_path, :string
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(raflle, attrs) do
+    raflle
+    |> cast(attrs, [:prize, :description, :ticket_price, :status, :image_path])
+    |> validate_required([:prize, :description, :ticket_price, :status, :image_path])
+  end
+end
